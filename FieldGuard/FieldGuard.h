606f9fcd7c6eef7da928ce9c89414dca9493e283
Goksel Koksal <gokselkoksal@gmail.com>
2017-08-20 13:20:53 +0300

    Initial Commit

---
+++ FieldGuard/FieldGuard.h
@@ -0,0 +1,19 @@
+//
+//  FieldGuard.h
+//  FieldGuard
+//
+//  Created by Göksel Köksal on 20/08/2017.
+//  Copyright © 2017 GK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FieldGuard.
+FOUNDATION_EXPORT double FieldGuardVersionNumber;
+
+//! Project version string for FieldGuard.
+FOUNDATION_EXPORT const unsigned char FieldGuardVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FieldGuard/PublicHeader.h>
+
+
